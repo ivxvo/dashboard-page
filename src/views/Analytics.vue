786c6fa3305673fe2b441b60cbcdfd94dcc27947
ivxvo <ihalx@mail.ru>
2021-10-12 23:09:13 +0300

    текст первого графика

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,5 +1,49 @@
 <template>
-    <div style="text-align: center;">
-        <h2>{{ $route.path == "/" ? "Home" : $route.name.toUpperCase() }}</h2>
+    <div class="container">
+        <div class="greeting">
+            <span>Hi Filip,</span><br/>
+            <span>Checkout your latest projects and their progress</span>
+        </div>
+        <div class="horizontal"/>
+        <div class="wavy"></div>
     </div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+.container {
+    padding: 52px 0 0 70px;
+}
+
+.greeting {
+    font-family: AvenirN;
+}
+
+.greeting span:first-child {
+    font-size: 18px;
+    color: #37474f;
+}
+
+.greeting span {
+    font-size: 14px;
+    color: #78909c;
+}
+
+.horizontal {
+    margin-top: 24px;
+    width: 1170px;
+    height: 2px;
+    background-color: #dee3e7;
+}
+
+.wavy {
+    padding-top: 43px;
+}
+
+</style>
