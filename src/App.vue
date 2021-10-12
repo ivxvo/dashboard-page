a62402d486afca0f47eabbcc561d36e164950c93
ivxvo <ihalx@mail.ru>
2021-10-12 10:46:59 +0300

    реализован раздел пользователя

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,16 @@
       <div class="search">
         <Search></Search>
       </div>
-      <div class="user"></div>
+      <div class="user">
+        <img src="./assets/img/Bell.png" alt="">
+        <div class="circle notify">{{ numNotify }}</div>
+        <div class="vertical"/>
+        <Dropdown
+          :list="dataList"
+          @selected="onSelected"
+        ></Dropdown>
+        <div class="circle avatar">{{ symbol }}</div>
+      </div>
     </div>
     <div class="sidebar">
       <Sidebar></Sidebar>
@@ -23,12 +32,40 @@
 <script>
 import Sidebar from "./components/Sidebar.vue";
 import Search from "./components/Search.vue";
+import Dropdown from "./components/Dropdown.vue";
 
 export default {
   name: 'App',
   components: {
     Sidebar,
-    Search
+    Search,
+    Dropdown
+  },
+  data() {
+    return {
+      numNotify: 8,
+      username: "Filip J.",
+      dataList: [
+                {
+                    name: "Filip J.",
+                    id: 1
+                },
+                {
+                    name: "Tom B.",
+                    id: 2
+                }
+            ]
+    }
+  },
+  methods: {
+    onSelected(id) {
+      this.username = this.dataList.find(item => item.id == id).name;
+    }
+  },
+  computed: {
+    symbol() {
+      return this.username.charAt(0);
+    }
   }
 }
 </script>
@@ -54,7 +91,6 @@ export default {
 
 .header {
   grid-column: 1 / 3;
-  /* background-color: #1e88e5; */
   display: grid;
   grid-template-columns: 230px 1104px 216px;
   grid-template-rows: 70px;
@@ -84,6 +120,48 @@ export default {
 .search {
   display: flex;
   align-items: center;
+}
+
+.user {
+  display: flex;
+  align-items: center;
+}
+
+.circle {  
+  display: flex;
+  align-items: center;
+  justify-content: center;  
+  color: #ffff;
+  border-radius: 50%;
+  -moz-border-radius: 50%;
+	-webkit-border-radius: 50%;
+  font-family: "Open Sans";
+  font-weight: bold;
+}
+
+.notify {
+  position: relative;
+  top: -10px;
+  left: -10px;
+  font-size: 10px;
+  background-color: #1e88e5;
+  width: 18px;
+  height: 18px;
+}
+
+.avatar {
+  font-size: 16px;
+  background-color: #37474f;
+  width: 40px;
+  height: 40px;
+  margin-left: 14px;
+}
+
+.vertical {
+  width: 2px;
+  background-color: #a1b1c2;
+  height: 24px;
+  margin-left: 10px;
 }
 
 .sidebar {
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <select name="dropdownlist" class="dropdown" @change="onSelected">
+            <option
+                v-for="item in list"
+                :key="item.id"
+                :value="item.id"
+            >
+                {{ item.name }}
+            </option>
+        </select>
+    </div>
+</template>
+<script>
+export default {
+    name: "Dropdown",
+    props: {
+        list: Array
+    },
+    methods: {
+        onSelected(event) {
+            this.$emit("selected", event.target.value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.dropdown {
+    border: none;
+    background-color: #e0e0e0;
+    padding-left: 15px;
+    color: #37474f;
+    font: 14px AvenirN;
+    outline: none;
+}
+
+</style>
